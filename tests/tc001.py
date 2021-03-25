76556c676bd6cfceb559ddc91577ceda984bc63a
huanima <806261607@qq.com>
2021-03-25 19:21:13 +0800

    睡醒啦！

--- tests/tc001.py
+++ tests/tc001.py
@@ -11,6 +11,4 @@ payload = {
 response = requests.post('http://127.0.0.1/api/mgr/signin',
                          data=payload)
 
-# print(response)
 pprint.pprint(response.json())
-# pprint.pprint(response.json().decode())
