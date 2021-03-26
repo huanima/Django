9c3e02daf562887e147fcfe6896f5307dff2087c
huanima <806261607@qq.com>
2021-03-26 14:01:04 +0800

    吃饱了想睡觉想要巨獭

--- common/models.py
+++ common/models.py
@@ -23,15 +23,46 @@ class Medicine(models.Model):
 
 
 
-
+# 一对多
 class Order(models.Model):
     # 订单名
     name = models.CharField(max_length=200,null=True,blank=True)
     # 创建日期
     create_date = models.DateTimeField(default=datetime.datetime.now)
     # 客户
+    # ForeignKey
     # on_delete:当这歌客户信息被删除时怎么办。
     # PROTECT:只有当这个customer对应的所有Order都被删除时，才可以删除这个customer
     # SET_NULL:删除这个客户信息后,Order里 customer = null（也就要设置Order的customer的null=True
     # CASCADE:如果客户信息被删除，删除所有链接的类的对应部分
     customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
+
+
+
+
+# 一对一（多用于说明）
+class ContactAddress(models.Model):
+    # OneToOneField  一对一
+    customer = models.OneToOneField(Customer,on_delete=models.PROTECT)
+    homeAddress = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
